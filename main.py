41cecfdd613ee6ad92b87c912e21239cafb5c249
Landon Fuhr <landonfuhr@gmail.com>
2020-07-19 14:23:35 -0600

    e

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ import time
 
 def main():
     with picamera.PiCamera() as camera:
-        camera.resolution = (1280, 720)
-        camera.framerate = 30
-        # Wait for the automatic gain control to settle
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
         time.sleep(2)
-        # Now fix the values
-        camera.shutter_speed = camera.exposure_speed
-        camera.exposure_mode = 'off'
-        g = camera.awb_gains
-        camera.awb_mode = 'off'
-        camera.awb_gains = g
-        # Finally, take several photos with the fixed settings
-        camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
+        camera.capture('foo.jpg')
 
 if __name__ == "__main__":
     main()
