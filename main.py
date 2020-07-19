c35e1a8c80bbb36e40788f375d6fc58dc95c7779
Landon Fuhr <landonfuhr@gmail.com>
2020-07-19 15:11:02 -0600

    q

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import picamera
 import time
 
-def main():
-    with picamera.PiCamera() as camera:
-        print('main')
-        camera.shutter_speed = 1000
-        camera.resolution = (1024, 768)
-        camera.start_preview()
-        # Camera warm-up time
-        time.sleep(2)
-        print('capturing')
-        camera.capture('foo.jpg')
-
-if __name__ == "__main__":
-    main()
+with picamera.PiCamera() as camera:
+    print('main')
+    camera.shutter_speed = 1000
+    camera.resolution = (1024, 768)
+    # Camera warm-up time
+    time.sleep(2)
+    print('capturing')
+    camera.capture('foo.jpg')
