cb913cceba252388b6a438f08be452a3c4175b18
Landon Fuhr <landonfuhr@gmail.com>
2020-07-19 21:00:30 -0600

    q

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from picamera import PiCamera
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
+import imutils
 import time
 import cv2
 
 def main():
-    with PiCamera() as camera:
-        camera.resolution = (1024, 768)
+    vs = PiVideoStream().start()
+    time.sleep(2.0)
+    fps = FPS().start()
+    # loop over some frames...this time using the threaded stream
+    while True:
+        frame = vs.read()
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        fps.update()
         time.sleep(2)
-        for frame in camera.capture_continuous():
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
 
 if __name__ == "__main__":
     main()
