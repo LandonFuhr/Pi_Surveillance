26946377e46f31dcb970a9982fc06583717403eb
Landon Fuhr <landonfuhr@gmail.com>
2020-07-19 20:57:29 -0600

    q

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
-# import the necessary packages
-from imutils.video.pivideostream import PiVideoStream
-from imutils.video import FPS
-import argparse
-import imutils
+from picamera import PiCamera
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-	help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-	help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
+def main():
+    with PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        time.sleep(2)
+        for frame in camera.capture_continuous():
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
 
-# created a *threaded *video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from `picamera` module...")
-vs = PiVideoStream().start()
-time.sleep(2.0)
-fps = FPS().start()
-# loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-    fps.update()
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+if __name__ == "__main__":
+    main()
