3b2e461c80b163a63d6606ccf66c9b658921269d
Landon Fuhr <landonfuhr@gmail.com>
2020-07-19 21:09:12 -0600

    q

--- main.py
+++ main.py
@@ -8,25 +8,28 @@ def main():
     targetFramerate = 20
 
     sleepLength = 1/targetFramerate
-    vs = PiVideoStream(resolution=(1024,768)).start()
+    vs = PiVideoStream(resolution=(640,480)).start()
     time.sleep(2.0)
     fps = FPS().start()
     # loop over some frames...this time using the threaded stream
-    while True:
-        frame = vs.read()
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-        fps.update()
-        if key == ord('q'):
-            break
-        time.sleep(sleepLength)
-    # stop the timer and display FPS information
-    fps.stop()
-    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-    # do a bit of cleanup
-    cv2.destroyAllWindows()
-    vs.stop()
+    try:
+        while True:
+            frame = vs.read()
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
+            fps.update()
+            if key == ord('q'):
+                break
+            # time.sleep(sleepLength)
+        # stop the timer and display FPS information
+        fps.stop()
+        print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    finally:
+        # do a bit of cleanup
+        cv2.destroyAllWindows()
+        vs.stop()
 
 if __name__ == "__main__":
     main()
