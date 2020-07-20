cff1b814a97c48ba60118716334f4c3acaf71b2b
Landon Fuhr <landonfuhr@gmail.com>
2020-07-19 18:50:27 -0600

    q

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import picamera
 import time
 
 with picamera.PiCamera() as camera:
-    print('main')
-    camera.shutter_speed = 1000
-    camera.resolution = (1024, 768)
     # Camera warm-up time
     time.sleep(2)
     print('capturing')
